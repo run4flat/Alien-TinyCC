1aee8a05a8ac0a5ec826383ea4a62187dfde74f9
David Mertens <dcmertens.perl@gmail.com>
2013-08-21 09:44:10 -0400

    Version bump

--- lib/Alien/TinyCC.pm
+++ lib/Alien/TinyCC.pm
@@ -1,7 +1,7 @@
 package Alien::TinyCC;
 
 # Follow Golden's Version Rule: http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 $VERSION = eval $VERSION;
 
 use File::ShareDir;
