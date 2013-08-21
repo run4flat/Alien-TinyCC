55c1bbd8146268bf9c56e3a7bbe2766899ca76e2
David Mertens <dcmertens.perl@gmail.com>
2013-08-21 10:35:20 -0400

    Fixed stupid semicolon issue for Windows build code

--- inc/My/Build/Windows.pm
+++ inc/My/Build/Windows.pm
@@ -42,14 +42,14 @@ sub patch_build_tcc_bat {
 		if ($line =~ /PROCESSOR_ARCH/) {
 			# Eat next line, too
 			<$in_fh>;
-			print $out_fh <<'EOF'
+			print $out_fh <<'EOF';
 @FOR /F "delims=" %%i IN ('perl -MConfig -e "$_=$Config{archname}; m/^MSWin32-(.*?)-/; print $1"') DO set TMP_PERLARCH=%%i}, "\n";
 @if %TMP_PERLARCH%==x64 goto x86_64
 EOF
 			next LINE;
 		}
 		if ($line =~ /\@set CC=x86_64/) {
-			print $out_fh "\@set CC=gcc -O0 -s -fno-strict-aliasing\n"
+			print $out_fh "\@set CC=gcc -O0 -s -fno-strict-aliasing\n";
 			next LINE;
 		}
 		print $out_fh $line;
