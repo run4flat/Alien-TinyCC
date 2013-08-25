13d35ec58623253f0efe4ede60f36b39e98b256b
David Mertens <dcmertens.perl@gmail.com>
2013-08-25 19:59:34 -0400

    Version bump for next full release

--- lib/Alien/TinyCC.pm
+++ lib/Alien/TinyCC.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # Follow Golden's Version Rule: http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-our $VERSION = "0.02_06";
+our $VERSION = "0.03";
 $VERSION = eval $VERSION;
 
 use File::ShareDir;
